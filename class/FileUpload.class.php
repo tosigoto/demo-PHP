3aa0df9d164f69f87e8845e3ce5137391a74bcf9
tosigoto <139558965+tosigoto@users.noreply.github.com>
2023-12-06 17:32:27 +0900

    Update FileUpload.class.php

--- class/FileUpload.class.php
+++ class/FileUpload.class.php
@@ -25,52 +25,17 @@ class FileUpload
 		/**
 		 * @var string アップロードされたファイルの temporary name
 		 */
-		$tmpName = isset($_FILES[$paramName]["tmp_name"]) ? //
-		$_FILES[$paramName]["tmp_name"] : //
-		null;
-
+		$tmpName = $_FILES[$paramName]["tmp_name"] ?? null;
 		if ($tmpName !== null && is_uploaded_file($tmpName) === true)
 		{
-			/**
-			 * @var string アップロードされたファイルのファイル名
-			 */
-			$fileName = isset($_FILES[Config::UPLOAD_FILE_PARAM_NAME]["name"]) ? $_FILES[Config::UPLOAD_FILE_PARAM_NAME]["name"] : null;
-			if ($fileName != null)
-			{
-				$filePath = "$destinationDir/$fileName";
-				if (move_uploaded_file($tmpName, $filePath) === true)
-				{
-					$resultAry['ok'] = true;
-					$resultAry['filePath'] = $filePath;
-
-					$is_chmod_ok = chmod($filePath, $permissions);
-					$permStringDec = decoct($permissions);
-
-					if ($is_chmod_ok === true)
-					{
-						$resultAry['msg'] = "$fileName をアップロードしました。パーミッションを$permStringDec に変更しました。\n";
-					}
-					else
-					{
-						$resultAry['msg'] = "$fileName をアップロードしましたが、パーミッションを$permStringDec に変更できませんでした。\n";
-					}
-				}
-				else
-				{
-					$resultAry['ok'] = false;
-					$resultAry['msg'] = "ファイルをアップロードできませんでした。($tmpName)";
-				}
-			}
-			else
-			{
-				$resultAry['ok'] = false;
-				$resultAry['msg'] = "ファイル名を取得できませんでした。\n($tmpName)";
-			}
+			$resultAry['ok'] = true;
+			$resultAry['msg'] = 'ファイルをアップロードしました。';
+			$resultAry['filePath'] = $tmpName;
 		}
 		else
 		{
 			$resultAry['ok'] = false;
-			$resultAry['msg'] = "ファイルが選択されていませんでした。\n($tmpName)";
+			$resultAry['msg'] = "ファイルが選択されていませんでした。";
 		}
 		return $resultAry;
 	}
